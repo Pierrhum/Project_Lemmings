3b8ed9d69d84d1b8887a66899048044b5d38a020
VictorCouturieux <victor.couturieux@laposte.net>
2022-09-24 21:32:45 +0200

    update animation class remove play_anim function and update constructor

--- ProjectLemming/Animation.h
+++ ProjectLemming/Animation.h
@@ -9,11 +9,13 @@ using namespace std;
 
 class Animation
 {
+private:
+    int currant_frame = 0;
+    
 public:
-    int nb_frames;
     Picture origin_picture;
+    int nb_frames;
     vector<Picture> vanim;
-    Animation(const char* str_file, int nb_frames);
-    void play_anim(std::vector<std::vector<CHAR_INFO>> &buffer, int cpt, bool loop);
+    Animation(const char* str_file, int nb_frames, bool flipped = false);
     Picture get_frame(int num_fram);
 };
