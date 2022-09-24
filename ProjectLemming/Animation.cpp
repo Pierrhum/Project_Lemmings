bc5baf2841e87ca8ca17063345b2d4cbe77e6e4a
VictorCouturieux <victor.couturieux@laposte.net>
2022-09-24 21:32:45 +0200

    update animation class remove play_anim function and update constructor

--- ProjectLemming/Animation.cpp
+++ ProjectLemming/Animation.cpp
@@ -1,29 +1,20 @@
 ﻿#include "Animation.h"
 
-Animation::Animation(const char* str_file, int nb_frames): nb_frames(nb_frames), origin_picture(Picture(str_file))
+Animation::Animation(const char* str_file, int nb_frames, bool flipped): origin_picture(Picture(str_file)), nb_frames(nb_frames)
 {
     vector<int> frame_buffer;
+    if (flipped) origin_picture.flip_picture();
     for (int i = 0; i < origin_picture.h_picture * origin_picture.w_picture; ++i)
     {
-        frame_buffer.push_back(origin_picture.vpicture.at(i));
-        if (i >= (vanim.size()+1)*(origin_picture.h_picture/nb_frames)*origin_picture.w_picture)
+        frame_buffer.push_back(origin_picture.get_pixel(i));
+        
+        if (i >= (vanim.size()+1)*(origin_picture.h_picture/nb_frames)*origin_picture.w_picture-1)
         { 
             vanim.push_back(Picture(frame_buffer, origin_picture.w_picture, origin_picture.h_picture/nb_frames));
             frame_buffer.clear();
         }
     }
     
-}
-
-void Animation::play_anim(std::vector<std::vector<CHAR_INFO>> &buffer, int cpt, bool loop)
-{   
-    if (loop)
-    {
-        DrawLemming::Instance().DrawPicture(buffer, 0, 0, vanim.at(cpt));
-    }else
-    {
-        DrawLemming::Instance().DrawPicture(buffer, 0, 0, vanim.at(cpt));
-    }
 }
 
 Picture Animation::get_frame(int num_fram)
