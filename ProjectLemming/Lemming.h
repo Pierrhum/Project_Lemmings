89e557e17216deba150f2c96f40557a0120114f4
VictorCouturieux <victor.couturieux@laposte.net>
2022-09-24 21:38:05 +0200

    upgrade lemming class with a coord, state of anim, can play his anims and can move on screen

--- ProjectLemming/Lemming.h
+++ ProjectLemming/Lemming.h
@@ -1,10 +1,28 @@
 ﻿#pragma once
 #include "Animation.h"
 
+enum State
+{
+    RMOVE = 0, LMOVE = 1, FALL = 2, DIG = 3
+};
+
+struct Movement
+{
+    State state;
+    COORD lem_vector;
+};
+
 class Lemming
 {
 public:
-    Animation move = Animation("spriteAscii/lem_move.txt", 8);
-    // Animation fall = Animation("spriteAscii/lem_fall.txt", 8);
-    Lemming();
+    vector<Animation*> animations;
+    Movement movements[4] = {{RMOVE, {1, 0}}, {LMOVE, {-1, 0}}, {FALL, {0, 1}}, {DIG, {0, 1}}};
+    COORD POS;
+    State currant_state = RMOVE;
+    int next_frame_to_play = 0;
+    Lemming(vector<Animation*> animation, COORD POS) : animations(animation), POS(POS){}
+    void play_next_frame(std::vector<std::vector<CHAR_INFO>> &buffer);
+    void change_state(State newState);
+    State get_state();
+    COORD get_center();
 };
