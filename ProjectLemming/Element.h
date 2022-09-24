f627b805b55988cd99f55b84c42f913345e70aaa
VictorCouturieux <victor.couturieux@laposte.net>
2022-09-24 21:40:48 +0200

    creat element class  who can animated picture on screen

---
+++ ProjectLemming/Element.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "Animation.h"
+
+class Element
+{
+public:
+    Animation* anim;
+    COORD POS;
+    int loop;
+    bool end_anim = false;
+    int next_frame_to_play = 0;
+    Element(Animation* anim, COORD POS, int loop = true) : anim(anim), POS(POS), loop(loop){}
+    void play_next_frame(std::vector<std::vector<CHAR_INFO>>& buffer);
+    COORD get_center();
+};
