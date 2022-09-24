6727b1e8a0fe762e944ddcdca4f5b30a506dc8d9
VictorCouturieux <victor.couturieux@laposte.net>
2022-09-24 21:35:05 +0200

    update DrawLeming class how can read  transparents pixels

--- ProjectLemming/DrawLemming.cpp
+++ ProjectLemming/DrawLemming.cpp
@@ -1,15 +1,16 @@
 ﻿#include "DrawLemming.h"
 
-#include <fstream>
-#include <string>
-
 void DrawLemming::DrawPixel(std::vector<std::vector<CHAR_INFO>> &buffer, int x, int y, Hexa_color color)
 {
+    
     int top_mask = 0xF0; int bot_mask = 0x0F;
     if(y > 97) return;
-    buffer[y/2][x].Attributes &= y%2 ? bot_mask : top_mask;
-    buffer[y/2][x].Attributes += y%2 ? color<<4 : color;
-    
+    if (color != -1)
+    {
+        buffer[y/2][x].Attributes &= y%2 ? bot_mask : top_mask;
+        buffer[y/2][x].Attributes += y%2 ? color<<4 : color;
+    }
+
     buffer[y/2][x].Char.UnicodeChar = 0x2580;
 }
 
@@ -17,6 +18,11 @@ void DrawLemming::DrawPicture(std::vector<std::vector<CHAR_INFO>> &buffer, int x
 {
     for (int i = 0; i < picture.h_picture*picture.w_picture; ++i)
     {
-        Instance().DrawPixel(buffer, x + i%picture.w_picture, y + i/picture.w_picture, (Hexa_color)picture.vpicture.at(i));
+        Instance().DrawPixel(buffer, x + i%picture.w_picture, y + i/picture.w_picture, (Hexa_color)picture.get_pixel(i));
     }
+}
+
+void DrawLemming::Refresh_init_level(std::vector<std::vector<CHAR_INFO>> &buffer)
+{
+    Instance().DrawPicture(buffer, 0, 0, intial_level);
 }
