0b047bdb6e25b465a6af060aa14aa9686ddc561e
Pierrhum <34352456+Pierrhum@users.noreply.github.com>
2022-10-26 20:28:38 +0200

    Play  & Quit button assets

--- ProjectLemming/DrawLemming.h
+++ ProjectLemming/DrawLemming.h
@@ -21,7 +21,7 @@ class DrawLemming
 {
 public:
     DrawLemming() :
-    Play(new Animation("spriteAscii/button/DigButton.txt", 2), COORD{150, 60}, UIButton::PLAY),
+    Play(new Animation("spriteAscii/button/playButton.txt", 1), COORD{130, 50}, UIButton::PLAY),
     dig_button(DIG_BUTTON, COORD{0, 84}),
     drop(new Animation("spriteAscii/drop/drop16.txt", 10), COORD{50, 20}, false),
     door(new Animation("spriteAscii/door/door16.txt", 6), COORD{110, 67}, true), // 145,67
